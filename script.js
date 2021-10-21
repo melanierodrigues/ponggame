cc7179da54c6c4130ebf91afe8f37a4271cf3c3a
Mélanie Rodrigues <melanierodrigues.mail@gmail.com>
2021-10-21 11:26:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -551,7 +551,7 @@ observer = new IntersectionObserver ((entries) => {
         function hello1() {
             if(entry.intersectionRatio > 0) { //screen.width < 480 && screen.height < 850
                 //document.documentElement.requestFullscreen();
-                document.onfullscreenchange;
+                document.documentElement.onfullscreenchange();
             }
         }
         requestAnimationFrame(hello1);
