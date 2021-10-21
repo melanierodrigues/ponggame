c4cf424b3110fa562cb2f399684bc766d1b4ab44
Mélanie Rodrigues <melanierodrigues.mail@gmail.com>
2021-10-21 10:52:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -282,8 +282,6 @@ function inicializa(){
     vPainelPontosCpu = document.getElementById("txtPontosCpu");
     document.addEventListener("keydown", teclaDw);
     document.addEventListener("keyup", teclaUp);
-
-    mobileFullScreen();
 }
 
 
@@ -546,6 +544,24 @@ function mobileFullScreen() {
     if (screen.width < 480 && screen.height < 850) {
         document.documentElement.requestFullscreen();
     } 
+}
+
+
+window.onload = maxWindow;
+
+function maxWindow() {
+    window.moveTo(0, 0);
+
+    if (document.all) {
+        top.window.resizeTo(screen.availWidth, screen.availHeight);
+    }
+
+    else if (document.layers || document.getElementById) {
+        if (top.window.outerHeight < screen.availHeight || top.window.outerWidth < screen.availWidth) {
+            top.window.outerHeight = screen.availHeight;
+            top.window.outerWidth = screen.availWidth;
+        }
+    }
 }
 
 
